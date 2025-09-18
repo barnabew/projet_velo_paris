d64d57ca11b16d9d86e48391f85d9dc96ec5ed39
barnabew <barnabe.willenbucher@outlook.fr>
2025-09-18 16:01:54 +0200

    Update app_streamlit.py

--- app_streamlit.py
+++ app_streamlit.py
@@ -8,7 +8,7 @@
 # https://opendata.paris.fr/explore/dataset/comptage-velo-donnees-compteurs/
 # Elles ont était prise entre le 01/09/2024 et le 31/08/2025
 
-pip install --upgrade kaleido
+import kaleido
 
 # === 1. Importation des librairies ===
 import numpy as np
