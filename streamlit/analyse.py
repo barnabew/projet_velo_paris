66a630ef08f05642e79e21088f3c9c858a74eaff
barnabew <barnabe.willenbucher@outlook.fr>
2025-10-01 15:13:35 +0200

    Update analyse.py

--- streamlit/analyse.py
+++ streamlit/analyse.py
@@ -7,41 +7,41 @@ def moyennes(df):
           0: "Lundi", 1: "Mardi", 2: "Mercredi",
           3: "Jeudi", 4: "Vendredi", 5: "Samedi", 6: "Dimanche"
       }
-
+     
      mois_fr = {
           1: "Janvier",2: "Février",3: "Mars",4: "Avril",
           5: "Mai",6: "Juin",7: "Juillet",8: "Août",
           9: "Septembre",10: "Octobre",11: "Novembre",12: "Décembre"
       }
-
+     
      jours_par_mois = (
           df.groupby("Mois_num")["Date et heure de comptage"]
             .apply(lambda x: x.dt.date.nunique())
             .reset_index(name="Nb_jours")
      )
-
-    
-    moyenne_heure = (
+     
+     
+     moyenne_heure = (
         (df.groupby("Heure")["Comptage horaire"].sum() / 365)
         .reset_index().round(1)
         .sort_values(by="Heure")
-    )
-    moyenne_jour = (
+     )
+     moyenne_jour = (
         (df.groupby("Jour_num")["Comptage horaire"].sum() / 52)
         .reset_index().round(1)
         .sort_values(by="Jour_num", ascending=True)
-    )
-    
-    moyenne_jour["Jour"] = moyenne_jour["Jour_num"].map(jours_fr)
-
-    somme_mois = df.groupby("Mois_num")["Comptage horaire"].sum().reset_index(name="Total_velos")
-    moyenne_mois = somme_mois.merge(jours_par_mois, on="Mois_num")
-    moyenne_mois["Moyenne_jour"] = (moyenne_mois["Total_velos"] / moyenne_mois["Nb_jours"]).round(1)
-
-    moyenne_mois["Mois"] = moyenne_mois["Mois_num"].map(mois_fr)
-    
-    int_heure = df.groupby(["Nom du site de comptage","lat","lon","Heure"]).agg(Velos=("Comptage horaire","sum")).reset_index()
-    return moyenne_heure, moyenne_jour, moyenne_mois, int_heure
+     )
+     
+     moyenne_jour["Jour"] = moyenne_jour["Jour_num"].map(jours_fr)
+     
+     somme_mois = df.groupby("Mois_num")["Comptage horaire"].sum().reset_index(name="Total_velos")
+     moyenne_mois = somme_mois.merge(jours_par_mois, on="Mois_num")
+     moyenne_mois["Moyenne_jour"] = (moyenne_mois["Total_velos"] / moyenne_mois["Nb_jours"]).round(1)
+     
+     moyenne_mois["Mois"] = moyenne_mois["Mois_num"].map(mois_fr)
+     
+     int_heure = df.groupby(["Nom du site de comptage","lat","lon","Heure"]).agg(Velos=("Comptage horaire","sum")).reset_index()
+     return moyenne_heure, moyenne_jour, moyenne_mois, int_heure
 
 
 def kpi_moyennes(df):
