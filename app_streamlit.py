c08b3fd634b0bd286b74340d7f84c03c23d2fec7
barnabew <barnabe.willenbucher@outlook.fr>
2025-10-01 13:27:12 +0200

    Update app_streamlit.py

--- app_streamlit.py
+++ app_streamlit.py
@@ -3,7 +3,7 @@ from data import chargement_nettoyage
 from analyse import kpi_moyennes, moyennes
 from visuel import plot_heure, plot_jour, plot_mois, plot_carte
 from textes import textes
-
+from pdf import create_pdf,plotly_to_png_bytes
 
 # === Streamlit App ===
 
@@ -56,7 +56,16 @@ with onglets[0]:
     # Texte résumé à droite
     with col1:
         style_textes(textes["resume"])
-
+        if st.button("Générer PDF complet"):
+            pdf_buffer = create_pdf(kpis, moyenne_heure, moyenne_jour, moyenne_mois, int_heure)
+            st.download_button(
+                label="Télécharger le PDF",
+                data=pdf_buffer,
+                file_name="rapport_velo.pdf",
+                mime="application/pdf"
+            )
+            st.success("PDF prêt au téléchargement !")
+            
 # --- Analyse par heure ---
 with onglets[1]:
     st.header("Moyenne des vélos par heure")
@@ -92,5 +101,6 @@ with onglets[4]:
         st.plotly_chart(plot_carte(int_heure, heure=17), use_container_width=True)
     with col1:
         style_textes(textes["carte"])
+
 
     
